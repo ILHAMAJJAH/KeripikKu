e36cbd25cd036498ff291a80eb511cb7f6320463
PANJI <144399081+ILHAMAJJAH@users.noreply.github.com>
2024-09-09 11:30:12 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -24,14 +24,14 @@ const sr = ScrollReveal({
   reset: true,
 });
 
-sr.reveal(".home-text", { delay: 300 });
-sr.reveal(".home-img", { delay: 400 });
-sr.reveal(".container", { delay: 400 });
+// sr.reveal(".home-text", { delay: 300 });
+// sr.reveal(".home-img", { delay: 400 });
+// sr.reveal(".container", { delay: 400 });
 
-sr.reveal(".about-img", {});
-sr.reveal(".about-text", { delay: 300 });
+// sr.reveal(".about-img", {});
+// sr.reveal(".about-text", { delay: 300 });
 
-sr.reveal(".middle-text", {});
-sr.reveal(".row-btn,.shop-content", { delay: 300 });
+// sr.reveal(".middle-text", {});
+// sr.reveal(".row-btn,.shop-content", { delay: 300 });
 
-sr.reveal(".review-content,.contact", { delay: 300 });
+// sr.reveal(".review-content,.contact", { delay: 300 });
